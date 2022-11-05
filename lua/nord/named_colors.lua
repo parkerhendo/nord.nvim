d2b89f0a18ed9133332f75974c822b7df9217412
Parker Henderson <parker@parkerhendo.com>
2022-11-04 19:36:38 -0700

    reverted theme colors to original

--- lua/nord/named_colors.lua
+++ lua/nord/named_colors.lua
@@ -1,17 +1,17 @@
 local nord = {
 	--16 colors
-	black = "#1f2228", -- nord0 in palette
+	black = "#1F2228", -- nord0 in palette
 	dark_gray = "#2e3440", -- nord1 in palette
 	gray = "#3b4252", -- nord2 in palette
 	light_gray = "#434c5e", -- nord3 in palette
 	light_gray_bright = "#616E88", -- out of palette
-	darkest_white = "#E5E6E6", -- nord4 in palette
-	darker_white = "#DDDFDF", -- nord5 in palette
-	white = "#E8EAED", -- nord6 in palette
+	darkest_white = "#D8DEE9", -- nord4 in palette
+	darker_white = "#E5E9F0", -- nord5 in palette
+	white = "#ECEFF4", -- nord6 in palette
 	teal = "#8FBCBB", -- nord7 in palette
 	off_blue = "#88C0D0", -- nord8 in palette
-	glacier = "#88C0D0", -- nord9 in palette // duplicate of the above color so I don't need to edit the rest of the theme def
-	blue = "#88C0D0", -- nord10 in palette   // duplicate of the above color so I don't need to edit the rest of the theme def
+	glacier = "#81A1C1", -- nord9 in palette // duplicate of the above color so I don't need to edit the rest of the theme def
+	blue = "#5E81AC", -- nord10 in palette   // duplicate of the above color so I don't need to edit the rest of the theme def
 	red = "#BF616A", -- nord11 in palette
 	orange = "#D08770", -- nord12 in palette
 	yellow = "#EBCB8B", -- nord13 in palette
